2828c6d5ce6ec7c4eaf7e2dec0f1efb3f34381ea
Jose Luis Camacho <jose.luis244@gmail.com>
2019-11-06 21:23:08 -0400

    a

--- instancias.js
+++ instancias.js
@@ -12,7 +12,7 @@ module.exports.instanacias = function LeerInstancias(ID){
         con.query(`SELECT uid.internalId As INST_ID, uid.publicId As INST_UID, CAST(pos.value As UNSIGNED ) As POS, file.uuid As FILE 
                     FROM Resources uid
                     JOIN MainDicomTags pos ON pos.id=uid.internalId
-                    JOIN attachedfiles file ON file.id=uid.internalId
+                    JOIN AttachedFiles file ON file.id=uid.internalId
                     WHERE uid.resourceType=3 and uid.parentId=${ID}
                     AND pos.tagGroup=32 and pos.tagElement=19 
                     AND file.fileType=1
