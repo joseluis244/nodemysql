3a6529068f45779463bdb18b711d0dff20b555e0
Jose Luis Camacho <jose.luis244@gmail.com>
2019-11-06 21:00:09 -0400

    res

--- estudios.js
+++ estudios.js
@@ -13,11 +13,11 @@ module.exports.estudios = function LeerEstudios(inicio,final){
         con.connect()
         con.query(`SELECT fecha.id As ID, CAST(fecha.value as UNSIGNED) As FECHA, nombre.value As NOMBRE, sexo.value As SEXO, pas_id.value As PAS_ID,
             res.publicId As EST_UID
-            FROM maindicomtags fecha 
-            JOIN maindicomtags nombre ON fecha.id = nombre.id 
-            JOIN maindicomtags sexo ON sexo.id = fecha.id
-            JOIN maindicomtags pas_id ON pas_id.id = fecha.id
-            JOIN resources res ON res.internalId = fecha.id
+            FROM MainDicomTags fecha 
+            JOIN MainDicomTags nombre ON fecha.id = nombre.id 
+            JOIN MainDicomTags sexo ON sexo.id = fecha.id
+            JOIN MainDicomTags pas_id ON pas_id.id = fecha.id
+            JOIN Resources res ON res.internalId = fecha.id
             WHERE fecha.tagGroup=8 and fecha.tagElement=32 AND CAST(fecha.value as UNSIGNED) > ${inicio} AND CAST(fecha.value as UNSIGNED) <= ${final}
             AND nombre.tagGroup=16 AND nombre.tagElement=16 
             AND sexo.tagGroup=16 AND sexo.tagElement=64
@@ -38,8 +38,8 @@ module.exports.estudios = function LeerEstudios(inicio,final){
 function LeerSeries(con,ID){
     return new Promise((res,rej)=>{
         con.query(`SELECT serie.internalId As SER_UID, serie.publicId As SER_ID, modalidad.value As MODALIDAD 
-        FROM resources serie
-        JOIN maindicomtags modalidad ON modalidad.id = serie.internalId
+        FROM Resources serie
+        JOIN MainDicomTags modalidad ON modalidad.id = serie.internalId
         WHERE serie.parentId=${ID}
         AND modalidad.tagGroup=8 AND modalidad.tagElement=96`,(err,resp,field)=>{
             res(resp)
