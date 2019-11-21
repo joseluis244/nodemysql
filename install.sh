dc1681dea0ffa9cbcf8df7fc6eb2be6b2167ec6b
Jose Luis Camacho <jose.luis244@gmail.com>
2019-11-21 19:10:38 -0400

    a

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 sudo apt-get update -y
 sudo apt-get install orthanc nodejs npm mysql-server build-essential unzip cmake mercurial \ uuid-dev libcurl4-openssl-dev liblua5.3-dev \ libgtest-dev libpng-dev libsqlite3-dev libssl-dev libjpeg-dev \ zlib1g-dev libdcmtk-dev libboost-all-dev libwrap0-dev \ libcharls-dev libjsoncpp-dev libpugixml-dev locales -y
 sudo wget https://www.orthanc-server.com/downloads/get.php?path=/plugin-mysql/OrthancMySQL-2.0.tar.gz
-sudo tar -xvzf OrthancMySQL-2.0.tar.gz
+sudo tar -xvzf get.php?path=%2Fplugin-mysql%2FOrthancMySQL-2.0.tar.gz
