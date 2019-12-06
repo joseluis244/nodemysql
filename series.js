d19b70c4858d22006c2533a79fc5a934db77d80e
jose luis <jose.luis244@gmail.com>
2019-12-06 10:57:30 -0400

    i

--- series.js
+++ series.js
@@ -13,8 +13,8 @@ module.exports.series = function Series(ID){
         con.query(`SELECT uid.internalId As SER_ID,  uid.publicId As SER_UID, descr.value As DESCRIPCION
         FROM Resources uid
         JOIN MainDicomTags descr ON descr.id = uid.internalId
-        WHERE uid.parentId=${ID} and uid.resourceType=2
-        AND ( (descr.tagGroup=8 and descr.tagElement=4158) or (descr.tagGroup=24 and descr.tagElement=5120) ) `,(err,res)=>{
+        WHERE uid.parentId=${ID} and uid.resourceType=2`
+        /*AND ( (descr.tagGroup=8 and descr.tagElement=4158) or (descr.tagGroup=24 and descr.tagElement=5120) ) `*/,(err,res)=>{
             Pres(res)
             con.end()
         })
