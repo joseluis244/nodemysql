957925f6de68ae5b13cfc1ec200431c505c657ef
jose luis <jose.luis244@gmail.com>
2019-12-06 11:11:50 -0400

    a

--- series.js
+++ series.js
@@ -10,11 +10,15 @@ module.exports.series = function Series(ID){
             database: "medicaltec"
         });
         con.connect()
-        con.query(`SELECT uid.internalId As SER_ID,  uid.publicId As SER_UID, descr.value As DESCRIPCION
+        /*con.query(`SELECT uid.internalId As SER_ID,  uid.publicId As SER_UID, descr.value As DESCRIPCION
         FROM Resources uid
         JOIN MainDicomTags descr ON descr.id = uid.internalId
         WHERE uid.parentId=${ID} and uid.resourceType=2`
-        /*AND ( (descr.tagGroup=8 and descr.tagElement=4158) or (descr.tagGroup=24 and descr.tagElement=5120) ) `*/,(err,res)=>{
+        /*AND ( (descr.tagGroup=8 and descr.tagElement=4158) or (descr.tagGroup=24 and descr.tagElement=5120) ) `,(err,res)=>{
+            Pres(res)
+            con.end()
+        })*/
+        con.query(`select internalId As SER_ID, publicId As SER_UID from medicaltec.Resources where parentId = ${ID}`,(err,res)=>{
             Pres(res)
             con.end()
         })
